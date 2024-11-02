d4f14f0d13c73804f8f3eb2ee6d414e44c6e6efb
michaelleahy44 <mjleahy3@gmail.com>
2024-11-01 20:53:40 -0400

    Uploaded Project files

--- main.py
+++ main.py
@@ -5,30 +5,15 @@ import threading
 from pynput.mouse import Controller, Button
 from pynput.keyboard import Listener, KeyCode
 
-from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.maximize_window()
-driver.get("www.google.com")
-
-time.sleep(3)
-
-driver.refresh()
-
 TOGGLE_KEY = KeyCode(char="t")
 clicking = False
 mouse = Controller()
 
 
 def clicker():
-    count = 0;
     while True:
         if clicking:
             mouse.click(Button.left, 1)
-            count += 1
-            if count > 5000:
-                driver.refresh()
-                count = 0
         time.sleep(0.001)
 
 
